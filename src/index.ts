78885dda42d30b0479c94739fa66edf17220ce20
mvkenhov <michiel.vankenhove@ugent.be>
2024-03-11 17:21:52 +0100

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Elysia } from "elysia";
 import { initControllers } from "./controllers";
+import cors from "@elysiajs/cors";
 
-const app = new Elysia({ prefix: "/api" });
+const app = new Elysia({ prefix: "/api" }).use(cors());
 
 initControllers(app);
 
 app.listen(process.env.PORT ?? 3000, () => {
-  console.log(`MOZAIK-Obelisk API is running at ${app.server?.hostname}:${app.server?.port}`);
+  console.log(
+    `MOZAIK-Obelisk API is running at ${app.server?.hostname}:${app.server?.port}`,
+  );
 });
-
